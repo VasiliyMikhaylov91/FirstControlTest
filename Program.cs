732e6c69f58ba2223cced1e3cd0e6ba86b93a1a4
VaasiliyMikhaylov91 <chelchik91@mail.ru>
2022-08-04 16:32:31 +0700

    Add code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string [] startArray = {"Hello", "2", "world", ":-)"};
+
+string endString = String.Empty;
+
+int lengthLimit = 3;
+
+for (int i = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= lengthLimit) endString += startArray[i] + ";";
+}
+
+string[] endArray = endString.Split(";");
+
+Console.WriteLine($"[{String.Join(", ", endArray)}]");
